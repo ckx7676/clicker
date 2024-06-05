b8c8db390e16d929cda74687e81a5806f39b260b
ckx7676 <105156618+ckx7676@users.noreply.github.com>
2024-06-06 01:33:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,7 @@ def sleep(ms):
 
 
 def resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
 
